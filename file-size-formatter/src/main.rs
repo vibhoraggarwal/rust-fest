273e08fc65d45d39003900a7d237eb7d03eb3bd7
vibhoraggarwal <vibhorag24@gmail.com>
2025-09-09 23:26:38 +0200

    code file-size-formatter inherited from example

---
+++ file-size-formatter/src/main.rs
@@ -0,0 +1,27 @@
+enum FileSize {
+    Bytes(u64),
+    Kilobytes(f64),
+    Megabytes(f64),
+    Gigabytes(f64),
+}
+
+fn format_size(size: u64) -> String {
+    let filesize = match size {
+        0..=999 => FileSize::Bytes(size),
+        1000..=999_999 => FileSize::Kilobytes(size as f64 / 1000.0),
+        1_000_000..=999_999_999 => FileSize::Megabytes(size as f64 / 1_000_000.0),
+        _ => FileSize::Gigabytes(size as f64 / 1_000_000_000.0),
+    };
+
+    match filesize {
+        FileSize::Bytes(bytes) => format!("{} bytes", bytes),
+        FileSize::Kilobytes(kb) => format!("{:.2} KB", kb),
+        FileSize::Megabytes(mb) => format!("{:.2} MB", mb),
+        FileSize::Gigabytes(gb) => format!("{:.2} GB", gb),
+    }
+}
+
+fn main() {
+    let result = format_size(6888837399);
+    println!("{}", result)
+}
